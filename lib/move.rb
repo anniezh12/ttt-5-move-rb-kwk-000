985cbabc9cd8c5c2ed1df6a3e56d582193450b67
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-07-21 07:21:58 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,10 +13,10 @@ def input_to_index(user_input)
 end
 def move(array,index,value="X")
   array[index]=value
-  board = [" ", " ", " "]
+#  board = [" ", " ", " "]
   def update_array_at_with(array, index, value)
     array[index] = value
   end
-update_array_at_with(board, index, value)
+update_array_at_with(array, index, value)
 display_board(array)
 end
